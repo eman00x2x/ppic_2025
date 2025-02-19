4d36ce0a76e0ea476973007e1f025f842fca1c7d
eman00x2x <eman00x2xx@gmail.com>
2025-02-19 17:48:31 +0800

    .

--- Public/global_assets/js/main/script.js
+++ Public/global_assets/js/main/script.js
@@ -3,7 +3,7 @@ const app = function () {
 	"use strict";
 
 	const recordTraffic = () => {
-		const url = eo.DOMAIN + `/saveTraffic`;
+		const url = `https://www.philproperties.ph/saveTraffic`;
 		const pageTitle = document.title;
 		const pageUrl = window.location.href;
 		const {
