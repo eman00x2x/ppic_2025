1dd7106d9ef2020911dcf06dee4cb5eeacded428
eman00x2x <eman00x2xx@gmail.com>
2025-02-27 13:09:06 +0800

    .

--- Resources/Templates/authenticated/users/menu.php
+++ Resources/Templates/authenticated/users/menu.php
@@ -43,6 +43,23 @@ if(MAINTENANCE === false) {
 		$html[] = "</div>";
 	$html[] = "</li>";
 
+	if(can('view_articles')) {
+		$html[] = "<li class='nav-item '>";
+			$html[] = "<a class='nav-link dropdown-toggle' href='#' data-bs-toggle='dropdown' data-bs-auto-close='false' role='button' aria-expanded='false'>";
+				$html[] = "<i class='ti ti-book nav-link-icon d-lg-inline-block fs-22'></i>";
+				$html[] = "<span class='nav-link-title'>Articles</span>";
+			$html[] = "</a>";
+			$html[] = "<div class='dropdown-menu '>";
+				$html[] = "<div class='dropdown-menu-columns'>";
+					$html[] = "<div class='dropdown-menu-column'>";
+						$html[] = "<a class='dropdown-item' href='" . url('articles') . "'>Article List</a>";
+						$html[] = "<a class='dropdown-item' href='" . url('articles.add') . "'>Create Article</a>";
+					$html[] = "</div>";
+				$html[] = "</div>";
+			$html[] = "</div>";
+		$html[] = "</li>";
+	}
+
 	if(can('access_traffics')) {
 		/** TRAFFICS */
 		$html[] = "<li class='nav-item '>";
@@ -55,6 +72,28 @@ if(MAINTENANCE === false) {
 					$html[] = "<div class='dropdown-menu-column'>";
 						$html[] = "<a class='dropdown-item' href='" . url('traffics') . "'>Traffics</a>";
 					$html[] = "</div>";
+				$html[] = "</div>";
+			$html[] = "</div>";
+		$html[] = "</li>";
+	}
+
+	if(can('manage_settings')) {
+		$html[] = "<li class='nav-item '>";
+			$html[] = "<a class='nav-link dropdown-toggle' href='#' data-bs-toggle='dropdown' data-bs-auto-close='false' role='button' aria-expanded='false'>";
+				$html[] = "<i class='ti ti-settings nav-link-icon d-lg-inline-block fs-22'></i>";
+				$html[] = "<span class='nav-link-title'>Settings</span>";
+			$html[] = "</a>";
+			$html[] = "<div class='dropdown-menu '>";
+				$html[] = "<div class='dropdown-menu-columns'>";
+					
+					if(can('update_system_settings')) {
+						$html[] = "<a class='dropdown-item' href='" . url('settings', ['page' => 'system-settings']) . "'>System</a>";
+					}
+
+					if(can('update_web_settings')) {
+						$html[] = "<a class='dropdown-item' href='" . url('webSettings', ['page' => 'common-settings']) . "'>Web Settings</a>";
+					}
+
 				$html[] = "</div>";
 			$html[] = "</div>";
 		$html[] = "</li>";
